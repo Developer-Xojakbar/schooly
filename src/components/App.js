50323a7b08cb77d9f667f58b154cd1415853795f
Developer <unitygamesxojak@gmail.com>
2020-02-04 17:21:00 +0500

    Aside-0.1v

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from './Header';
+import Aside from './Aside';
+import Main from './Main';
 
 const App = () => {
     return (
         <React.Fragment>
-            
+            <Header />
+            <Aside />
+            <Main />
         </React.Fragment>
     );
 };
---
+++ src/components/Aside.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import IconSVG from './IconSVG';
+
+const Aside = () => {
+    return (
+        <div className="aside">
+            <h1 className="aside__logo">schooly</h1>
+            <p>Class Management</p>
+            <div className="aside__side-menu">
+                <IconSVG className="" iconName="icon-attendance" />
+                <p>Attendance</p>
+            </div>
+            <div className="aside__side-menu">
+                <IconSVG className="" iconName="icon-schedule" />
+                <p>Schedule</p>
+            </div>
+            <div className="aside__side-menu">
+                <IconSVG className="" iconName="icon-groups-students" />
+                <p>Groups and Students</p>
+            </div>
+            <div className="aside__side-menu">
+                <IconSVG className="" iconName="icon-messaging" />
+                <p>Messaging</p>
+            </div>
+            <p>Learning Managment</p>
+            <div className="aside__side-menu">
+                <IconSVG className="" iconName="icon-assignments" />
+                <p>Assignments</p>
+            </div>
+            <div className="aside__side-menu">
+                <IconSVG className="" iconName="icon-quizzes" />
+                <p>Quizzes</p>
+            </div>
+            <div className="aside__side-menu">
+                <IconSVG className="" iconName="icon-grade-book" />
+                <p>Grade Book</p>
+            </div>
+            <div className="aside__side-menu">
+                <img className="icon" src={require("../images/materials.png")} alt="Materials" />
+                <p>Materials</p>
+            </div>
+            <div className="aside__side-menu">
+                <IconSVG className="" iconName="icon-study-plan" />
+                <p>Study Plan</p>
+            </div>
+            <p>Personal</p>
+            <div className="aside__side-menu">
+                <IconSVG className="" iconName="icon-todos" />
+                <p>To-dos</p>
+            </div>
+            <div className="aside__side-menu">
+                <IconSVG className="" iconName="icon-finance" />
+                <p>Finance</p>
+            </div>
+            <div className="aside__side-menu">
+                <IconSVG className="" iconName="icon-profile" />
+                <p>Profile</p>
+            </div>
+        </div>
+    );
+};
+
+export default Aside;
