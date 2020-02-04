26979549d780b6d95051bb4cefd12febf2bb4d63
Developer <unitygamesxojak@gmail.com>
2020-02-04 23:01:47 +0500

    Header-0.1v

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="header">
+            <div className="header__menu-icon">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div className="header__menu-name">
+                <p>Class Management</p>
+                <p>Attendance</p>
+            </div>
+            <div className="header__switch-catalog">
+                {/*this "active" class is error */}
+                <div className="active">
+                    <div className="container">
+                        <p>Take attendance</p>
+                        <p>Attendance report</p>
+                    </div>
+                </div>
+                <p>Take attendance</p>
+                <p>Attendance report</p>
+            </div>
         </div>
     );
 };
